ff57e196c03a4eb9f311a2f5a6918e4b1424004d
borisd <b.desiatskii@corp.mail.ru>
2021-10-08 15:10:21 +0300

    3 replying modes

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"time"
 )
 
 const Alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+const (
+	ModeChoosing   = 0
+	ModeAlphabet   = 1
+	ModeCalculator = 2
+)
+
+var ModeWelcomeText = map[int]string{
+	ModeChoosing:   "Выберите режим: пришлите 1 для алфавитного, 2 для калькуляторного",
+	ModeAlphabet:   "Введите английскую букву, чтобы получить следующую",
+	ModeCalculator: "Введите цифры, чтобы получить сумму",
+}
 
 func main() {
 	tgtoken := os.Getenv("TGTOKEN")
@@ -19,8 +32,8 @@ func main() {
 		log.Panic(err)
 	}
 
-	daemon := Daemon{bot: bot}
 	dm := NewDelayMessage()
+	daemon := Daemon{bot: bot, chatMode: make(map[int64]int, 0), chatCalcSum: make(map[int64]int, 0), dm: &dm}
 	rand.Seed(time.Now().UnixNano())
 	//bot.Debug = false
 
@@ -41,26 +54,58 @@ func main() {
 			log.Println("Not a message")
 			continue
 		}
-		//handle(update.Message)
-		//go handle(update.Message)
-		daemon.handleWithSingleRsp(&dm, update.Message)
+		go daemon.handle(update.Message)
 	}
 }
 
 type Daemon struct {
-	bot *tgbotapi.BotAPI
+	bot         *tgbotapi.BotAPI
+	chatMode    map[int64]int
+	chatCalcSum map[int64]int
+	dm          *DelayMessage
 }
 
 func (d *Daemon) handle(message *tgbotapi.Message) {
 	log.Println("handle", message.Text)
-	d.send(message.Chat.ID, getNextLetter(message.Text))
+	chosenMode := ModeChoosing
+	if mode, ok := d.chatMode[message.Chat.ID]; ok {
+		log.Println("ok", message.Text)
+		if mode == ModeAlphabet {
+			log.Println("ok: ab", message.Text)
+			d.send(message.Chat.ID, getNextLetter(message.Text))
+			return
+		}
+
+		if mode == ModeCalculator {
+			log.Println("ok: calc", message.Text)
+			d.delayedSend(message.Chat.ID, d.calcNextSum(message.Chat.ID, message.Text))
+			return
+		}
+
+		if mode == ModeChoosing {
+			log.Println("ok: choos", message.Text)
+			chosenMode1, err := strconv.Atoi(message.Text)
+			chosenMode = chosenMode1 //hack
+			if err == nil && (chosenMode == ModeAlphabet || chosenMode == ModeCalculator) {
+				log.Println("ok: choos set", message.Text, chosenMode)
+				d.send(message.Chat.ID, fmt.Sprintf("Выбран режим %d", chosenMode))
+			} else {
+				log.Println("ok: else choos", message.Text)
+				chosenMode = ModeChoosing
+			}
+		}
+	}
+
+	log.Println("set chosen Mode", message.Text, chosenMode)
+	d.chatMode[message.Chat.ID] = chosenMode
+	d.send(message.Chat.ID, ModeWelcomeText[chosenMode])
 }
 
-func (d *Daemon) handleWithSingleRsp(dm *DelayMessage, message *tgbotapi.Message) {
-	log.Println("handleWithSingleRsp", message.Text)
+func (d *Daemon) delayedSend(chatID int64, text string) {
+	log.Println("delayedSend", text)
 	ld := rand.Intn(16000000000)
-	dm.SetLastDate(message.Chat.ID, ld)
-	go dm.ExecItem(d.send, message.Chat.ID, getNextLetter(message.Text), ld)
+	d.dm.SetLastDate(chatID, ld)
+	go d.dm.ExecItem(d.send, chatID, text, ld)
 }
 
 func (d *Daemon) send(chatId int64, text string) {
@@ -85,6 +130,16 @@ func getNextLetter(text string) string {
 	}
 
 	return string(reply)
+}
+func (d *Daemon) calcNextSum(chatID int64, text string) string {
+	nextVal, err := strconv.Atoi(text)
+	if err != nil {
+		return "Введите число"
+	}
+	d.chatCalcSum[chatID] += nextVal
+	reply := fmt.Sprintf("Текущая сумма %d", d.chatCalcSum[chatID])
+
+	return reply
 }
 
 type DelayMessage struct {
